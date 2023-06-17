c42e99849287f6dfe420b8d3cb6faa256bcf8064
Surya <suryaminu.jj@gmail.com>
2023-06-17 21:40:59 +0530

    Initial Commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,55 @@
+import Day from '../components/day';
+import '../styles/home.css';
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+
+export default function Home() {
+
+  const handleDayClick = (day: string) => {
+    if (day === 'Monday') {
+      window.location.href= '/list-item'
+    } else {
+      toast.info("You don't have any items for this day", {
+        position: toast.POSITION.TOP_CENTER
+      });
+    }
+  };
+
+  return (
+    <section className='home-container'>
+       <ToastContainer />
+      <div className='home-wrapper'>
+        <div className='heading'>
+          <h2>Things To Buy!</h2>
+        </div>
+        <div className='wrapper-home'>
+          <Day
+            style={{ backgroundColor: '#FFAACF' }}
+            onClick={() => handleDayClick('Monday')}
+          >
+            Monday
+          </Day>
+          <Day
+            style={{ backgroundColor: '#F6C391' }}
+            onClick={() => handleDayClick('Tuesday')}
+          >
+            Tuesday
+          </Day>
+          <Day
+            style={{ backgroundColor: '#6096B4' }}
+            onClick={() => handleDayClick('Thursday')}
+          >
+            Thursday
+          </Day>
+          <Day
+            style={{ backgroundColor: '#D9ACF5' }}
+            onClick={() => handleDayClick('Friday')}
+          >
+            Friday
+          </Day>
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ src/pages/login.tsx
@@ -0,0 +1,64 @@
+import '../styles/login.css';
+import { useState } from 'react';
+import { Auth } from '../core/Autentication';
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+// import {Location} from 'react-router-dom';
+
+export default function Login() {
+  const [credential, setCredential] = useState({
+    email: "",
+    password: "",
+  });
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setCredential((prevCredential) => ({
+      ...prevCredential,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event:  React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const dataAuth = {
+      email: credential.email,
+      password: credential.password,
+    }
+    const initAuth = new Auth(dataAuth);
+    const res = initAuth.login();
+    // const location = new Location()
+
+    if (res.status) {
+      toast.success(res.message, {
+        position: toast.POSITION.TOP_LEFT
+      });
+      setTimeout(() => {
+        window.location.href = "/home";     
+      }, 700)
+    } else {
+      toast.error(`${res.message} !`, {
+        position: toast.POSITION.TOP_LEFT
+      });
+    }
+  };
+
+  
+  return (
+    <section className='login-container'>
+      <h1>Please Login</h1>
+      <ToastContainer />
+      <form className='form-wrapper' onSubmit={handleSubmit}>
+        <div className='input-wrapper'>
+          <input type='email' required placeholder='Email Address' name='email' value={credential.email} onChange={handleInputChange} />
+          
+          <input type='password' required placeholder='Password' name='password' value={credential.password} onChange={handleInputChange} />
+        </div>
+        <div className='checkbox-wrapper'>
+          <input type='checkbox' name='' id='' className='' />
+          <label htmlFor=''>Remember me</label>
+        </div>
+        <button type='submit' >Login</button>
+      </form>
+    </section>
+  );
+}
